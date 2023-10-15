2e86e9a66e1591a507a6dbee787846c70cf5430b
Ebrahem Samer <ebrahemsamer2@gmail.com>
2023-10-16 01:28:33 +0300

    fix job form style

--- new_job.php
+++ new_job.php
@@ -11,34 +11,41 @@
                     <div class="col-lg-12 col-md-12">
                         <h3 class="mb-30">Create New Job</h3>
                         <form>
-                            <div class="mt-10">
+                            <div class="mt-10 pb-10">
                                 <input name="title" type="text" placeholder="Job Title"
                                     onfocus="this.placeholder = ''" onblur="this.placeholder = 'Job Title'" required
-                                    class="single-input">
+                                    class="form-control">
                             </div>
 
-                            <div class="mt-10">
-                                <textarea name="description" class="single-textarea" placeholder="Description" onfocus="this.placeholder=''"
+                            <div class="mt-10 pb-10">
+                                <textarea name="description" class="form-control" placeholder="Description" onfocus="this.placeholder=''"
                                     onblur="this.placeholder = 'Description'" required></textarea>
-                            </div>
-
-                            <div class="mt-10">
-                                <select class="single-input form-control" name="country">
-                                    <option value="0">Country</option>
-                                </select>
-                            </div>
-
-                            <div class="mt-10">
-                                <select class="single-input form-control" name="city">
-                                    <option value="0">City</option>
-                                </select>
                             </div>
 
                             <div class="container">
                                 <div class="row">
                                     <div class="col-md-6 p-0">
-                                        <div class="mt-10">
-                                            <select class="single-input form-control" name="job_nature">
+                                        <div class="mt-10 pb-10">
+                                            <select class="form-control" name="country">
+                                                <option value="0">Country</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6 pr-0">
+                                        <div class="mt-10 pb-10">
+                                            <select class="form-control" name="city">
+                                                <option value="0">City</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-6 p-0">
+                                        <div class="mt-10 pb-10">
+                                            <select class="form-control" name="job_nature">
                                                 <option value="Full Time">Full Time</option>
                                                 <option value="Part Time">Part Time</option>
                                                 <option value="Remotly">Remotly</option>
@@ -47,46 +54,67 @@
                                     </div>
 
                                     <div class="col-md-3 pr-0">
-                                        <div class="mt-10">
+                                        <div class="mt-10 pb-10">
                                         <input name="salary_from" type="text" placeholder="Salary From"
                                             onfocus="this.placeholder = ''" onblur="this.placeholder = 'Salary From'" required
-                                            class="single-input form-control">
+                                            class="form-control">
                                         </div>
                                     </div>
 
                                     <div class="col-md-3 pr-0">
-                                        <div class="mt-10">
+                                        <div class="mt-10 pb-10">
                                             <input name="salary_to" type="text" placeholder="Salary To"
                                                 onfocus="this.placeholder = ''" onblur="this.placeholder = 'Salary To'" required
-                                                class="single-input form-control">
+                                                class="form-control">
                                         </div>
                                     </div>
                                 </div>
                             </div>
                             
-                            <div class="mt-10">
-                                <textarea name="required_knowledge" class="single-textarea" placeholder="Required Knowledge: each should be seperated by semicolon(;)" onfocus="this.placeholder = ''"
-                                    onblur="this.placeholder = 'Required Knowledge: each should be seperated by semicolon(;)'" required></textarea>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-6 p-0">
+                                        <div class="mt-10 pb-10">
+                                            <textarea rows="4" name="required_knowledge" class="form-control" placeholder="Required Knowledge: each should be seperated by semicolon(;)" onfocus="this.placeholder = ''"
+                                                onblur="this.placeholder = 'Required Knowledge: each should be seperated by semicolon(;)'" required></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6 pr-0">
+                                        <div class="mt-10 pb-10">
+                                            <textarea rows="4" name="education_experience" class="form-control" placeholder="Education & Experience: each should be seperated by semicolon(;)" onfocus="this.placeholder = ''"
+                                                onblur="this.placeholder = 'Education & Experience: each should be seperated by semicolon(;)'" required></textarea>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
 
-                            <div class="mt-10">
-                                <textarea name="education_experience" class="single-textarea" placeholder="Education & Experience: each should be seperated by semicolon(;)" onfocus="this.placeholder = ''"
-                                    onblur="this.placeholder = 'Education & Experience: each should be seperated by semicolon(;)'" required></textarea>
+                            <div class="container">
+                                <div class="row">
+                                    
+                                    <div class="col-md-6 p-0">
+                                        <div class="mt-10 pb-10">
+                                            <input min="1" type="number" name="vacancy_number" placeholder="Vacancy Number"
+                                                onfocus="this.placeholder = ''" onblur="this.placeholder = 'Vacancy Number'" required
+                                                class="form-control">
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-6 pr-0">
+                                        <div class="mt-10 pb-10">
+                                            <select class="form-control" name="years_of_experience">
+                                                <option value="1-2">1-2</option>
+                                                <option value="2-3">2-3</option>
+                                                <option value="3-6">3-6</option>
+                                                <option value="6-more..">6-more..</option>
+                                            </select>
+                                        </div>
+                                    </div>
+
+                                    </div>
+                                </div>
                             </div>
 
-                            <div class="mt-10">
-                                <input min="1" type="number" name="vacancy_number" placeholder="Vacancy Number"
-                                    onfocus="this.placeholder = ''" onblur="this.placeholder = 'Vacancy Number'" required
-                                    class="single-input">
-                            </div>
-
-                            <div class="mt-10">
-                                <input min="1" type="number" name="years_of_experience" placeholder="Years of Experience"
-                                    onfocus="this.placeholder = ''" onblur="this.placeholder = 'Years of Experience'" required
-                                    class="single-input">
-                            </div>
-
-                            <div class="mt-10">
+                            <div class="mt-20 ml-3">
                                 <input class="btn head-btn1" type="submit" value="Create Job" name="create_job"/>
                             </div>
 
